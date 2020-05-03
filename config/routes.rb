a4879ad2810b7ca05db06f5268f0ea2288072bc9
Andrew Tran <andrewtttran@gmail.com>
2020-05-03 21:31:44 +1000

    edit/delete buttons work but can't get edit to look like a submit button

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,11 @@ Rails.application.routes.draw do
 
   get "/lunches/:id", to:"lunches#show", as: "lunch"
 
-  get "/lunches/:id/edit", to:"lunches#edit"
+  get "/lunches/:id/edit", to:"lunches#edit", as: "edit_lunch"
 
   post "/lunches", to:"lunches#create"
 
-  delete "/lunches", to:"lunches#destroy"
+  delete "/lunches/:id", to:"lunches#destroy"
 
   patch "/lunches/:id", to:"lunches#update"
 
