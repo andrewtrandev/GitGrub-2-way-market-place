0ed28c7bca43f1db04e37c5ee98b545f6e6b539e
Andrew Tran <andrewtttran@gmail.com>
2020-05-11 10:57:16 +1000

    finished authorisation for devise, added logout button

--- app/controllers/lunches_controller.rb
+++ app/controllers/lunches_controller.rb
@@ -1,8 +1,9 @@
 class LunchesController < ApplicationController
     before_action :authenticate_user!  #user can't do anything unless signed in
     skip_before_action :verify_authenticity_token
-    before_action :set_lunch, only: [:show, :destroy, :edit]
-    #before any methods get called set @lunch=Lunch.find(params[:id]) for the following methods
+    before_action :set_lunch, only: [:show] 
+    before_action :set_user_listing, only: [:edit, :update, :destroy] #users can only edit,update,destroy their own creations
+
 
 
     def index
@@ -23,7 +24,7 @@ class LunchesController < ApplicationController
         # @lunch=Lunch.create(name: params[:name], description: params[:description], price: params[:price])
         #above line of code doesn't work with new form - kept creating null records, but after setting listing_params and using below code it works.
         
-        @lunch=Lunch.create(lunch_params)
+        @lunch=current_user.lunches.create(lunch_params)
         if @lunch.errors.any? #is there any errors with creating lunch?
             render "new" #render new view
         else 
@@ -39,7 +40,7 @@ class LunchesController < ApplicationController
     end
 
     def update
-        @lunch=Lunch.update(params["id"], lunch_params)
+        @lunch=Lunch.update(params[:id], lunch_params)
         #note the format of this is important, params["id"] is the entry we're updating, lunch params are what we're updating
         if @lunch.errors.any? #is there any errors with updating lunch?
             render "edit" #render edit view
@@ -65,6 +66,15 @@ class LunchesController < ApplicationController
         @lunch=Lunch.find(params[:id])
         #using this method to dry up code as we use Lunch.find alot
         #called at the top
+    end
+
+    def set_user_listing
+        @lunch=current_user.lunches.find_by_id(params[:id])
+        #set @lunch to find the id of any lunches created by the current user
+
+        if @lunch == nil
+            redirect_to lunches_path #send user to index if can't be found
+        end
     end
 
 end
